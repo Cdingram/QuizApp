7198884c38d678c6a05ff28d67462b4d462af8e4
Cody Ingram <cdingram@ualberta.ca>
2016-08-17 14:16:09 -0600

    Added comments

--- quizapp/models.py
+++ quizapp/models.py
@@ -4,18 +4,21 @@ from django.db import models
 
 # Create your models here.
 
+# Main Quiz has date to define it and a name
 class Quiz(models.Model):
 	pub_date = models.DateTimeField('date published')
 	name = models.CharField(max_length=200)
 	def __str__(self):
 		return self.name
 
+# a question belongs to a quiz and has text that is the question itself
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     quiz = models.ForeignKey(Quiz)
     def __str__(self):
 		return self.question_text
 
+# answers are attached to questions, and can be the correct answer
 class Answer(models.Model):
     question = models.ForeignKey(Question)
     answer_text = models.CharField(max_length=200)
@@ -23,12 +26,14 @@ class Answer(models.Model):
     def __str__(self):
 		return self.answer_text
 
+# An attempt at the quiz. Each time the quiz is completed a new attempt is logged
 class QuizAttempt(models.Model):
 	pub_date = models.DateTimeField(auto_now_add=True)
 	quiz = models.ForeignKey(Quiz)
 	def __str__(self):
 		return self.quiz.name
 
+# these are the attempted answers, they relate to real answers and a real question, and are attached to a specific quiz attempt
 class AnswerAttempt(models.Model):
 	quiz_attempt = models.ForeignKey(QuizAttempt)
 	answer = models.ForeignKey(Answer)
--- quizapp/views.py
+++ quizapp/views.py
@@ -6,12 +6,15 @@ NUM_QUESTIONS = 12
 
 def index(request):
 
+	# get the quiz in order to grab questions, answers, and create quiz attempt
 	my_quiz = get_object_or_404(Quiz, name="Ultimate Quiz")
 
+	# get the questions
 	questions = get_list_or_404(Question, quiz=my_quiz)
 
 	answers = {}
 	
+	# make a dictionary with keys as questions and values as a list of answers
 	for each in questions:
 		answer = get_list_or_404(Answer, question=each)
 		answers[each] = answer 
@@ -20,6 +23,7 @@ def index(request):
 
 		score = 0
 
+		# POST is a submit, so create a quiz attempt
 		quizAttempt = QuizAttempt.objects.create(quiz=my_quiz)
 
 		for question in questions:
@@ -27,7 +31,8 @@ def index(request):
 			answers2 = get_list_or_404(Answer, question=question)
 
 			for answer in answers2:
-				
+
+				# if the answer is chosen, add it as an answer attempt on current quiz attempt. If its the correct answer, add to score
 				if request.POST["question"+str(question.id)] == answer.answer_text:
 
 					answerAttempt = AnswerAttempt.objects.create(quiz_attempt=quizAttempt, answer=answer, question=question)
@@ -36,10 +41,11 @@ def index(request):
 						score += 1
 
 
-
+		# render the page, post boolean will tell us to show the score and retry button
 		post = True
 		return render(request, 'quizapp/index.html', {'answers':answers, 'score':score, "num_questions": NUM_QUESTIONS, "post":post})
 
 	else:
+		# render page normally (not a POST request)
 		post = False
 		return render(request, 'quizapp/index.html', {'answers':answers, "num_questions": NUM_QUESTIONS, "post":post})
